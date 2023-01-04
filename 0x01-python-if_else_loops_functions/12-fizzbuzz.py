f42c2cdca9bc6f364e83d534c9de2a7ef92c6ef6
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-04 12:36:17 -0800

    inserting a c comp

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
     # For multiples of three, print Fizz instead of the number.
     # For multiples of five, print Buzz instead of the number.
     # For multiples of three and five, print FizzBuzz instead of the number.
-    for number in range(1, 100):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
