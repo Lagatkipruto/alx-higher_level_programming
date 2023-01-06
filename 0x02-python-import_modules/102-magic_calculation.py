f7d3d1dea1972a9cdbd3cc5f47caca3a97aa2781
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-06 01:12:26 -0800

    Bytecode python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    # Match bytecode provided by ALX program.
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
