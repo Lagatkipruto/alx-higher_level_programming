6de47d9384fedd8a808a00b0919ed1212d23e8c4
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-24 21:50:19 -0800

    Linkled list task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,11 +18,17 @@ class Square:
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
 
     @property
     def position(self):
@@ -38,18 +44,18 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+         """Return the current area of the square."""
+         return (self.__size * self.__size)
 
-        def my_print(self):
-            """Print the square with the # character."""
-            if self.__size == 0:
-                print("")
-                return
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
