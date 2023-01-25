29331b85c9144825041b9ecd891962037e803bf2
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-25 11:14:57 -0800

    Corrections on 2- safeprint list on setting of the position of return value

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,5 +17,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
-        print("")
+    print("")
     return (ret)
