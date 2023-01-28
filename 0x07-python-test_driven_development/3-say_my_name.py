ba5c53e5289289ee8b91065c463ecdd77afcfa10
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-28 05:12:23 -0800

    Python - Test-driven development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# Wilfred Lagat
+# Defines a name-printing function.
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): the last name to print.
+    Raises:
+        TypeError: If either of the first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This project is Python test driven development(TDD)
