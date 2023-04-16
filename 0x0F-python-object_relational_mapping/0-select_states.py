61561c9c0861f0b98c2734dea7e1ff2b00628df7
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-04-16 04:20:30 -0700

    Adding the port and the localhost

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306)
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                          host='localhost', port=3306)
     c = db.cursor()
