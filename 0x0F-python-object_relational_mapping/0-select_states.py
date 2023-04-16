55da52c7b39db587b1e3394048512717bb3bc62d
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-04-16 10:55:39 -0700

    Fixing double quotes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306)
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                         host='localhost', port=3306)
+                         host="localhost", port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]
