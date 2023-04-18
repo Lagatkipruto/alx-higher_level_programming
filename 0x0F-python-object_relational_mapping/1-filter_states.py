25b426f03c7f8b0e10f765fa21a84ce016823d48
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-04-18 08:39:35 -0700

    Adding more comments on task 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# This script lists all states\
-#        with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#        <mysql password> \
-#        <database name>
+""" This script lists all states\
+        with a name starting with N from the database hbtn_0e_0_usa.
+ Usage: ./1-filter_states.py <mysql username> \
+        <mysql password> \
+        <database name>
+"""
 import sys
 import MySQLdb
 
