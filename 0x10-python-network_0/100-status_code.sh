ff9983c84fc26729f903cdfd4156fd4200080e85
Lagatkipruto <kiprutowillie@gmail>
2023-04-27 12:55:01 -0700

    Python networking tasks

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# it sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+This project is about introduction to python network
